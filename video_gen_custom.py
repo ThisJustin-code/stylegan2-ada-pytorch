76abe29e2f904620c3a93943cac849a75ac172bf
unknown <gallagherjust71@gmail.com>
2021-04-22 14:45:51 -0400

    Small changes and updates

--- video_gen_custom.py
+++ video_gen_custom.py
@@ -1,8 +1,5 @@
 import sys
 sys.path.insert(0, "./")
-
-import sys
-sys.path.insert(0, "/content/stylegan2-ada-pytorch")
 import pickle
 import os
 import numpy as np
@@ -18,7 +15,6 @@ def seed2vec(G, seed):
   return np.random.RandomState(seed).randn(1, G.z_dim)
 
 def generate_image(G, z, truncation_psi):
-    # Render images for dlatents initialized from random seeds.
     Gs_kwargs = {
         'output_transform': dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True),
         'randomize_noise': False
@@ -27,62 +23,57 @@ def generate_image(G, z, truncation_psi):
         Gs_kwargs['truncation_psi'] = truncation_psi
 
     label = np.zeros([1] + G.input_shapes[1][1:])
-    images = G.run(z, label, **G_kwargs) # [minibatch, height, width, channel]
+    images = G.run(z, label, **G_kwargs)
     return images[0]
 
 def get_label(G, device, class_idx):
-  label = torch.zeros([1, G.c_dim], device=device)
-  if G.c_dim != 0:
-      if class_idx is None:
-          ctx.fail('Must specify class label with --class when using a conditional network')
-      label[:, class_idx] = 1
-  else:
-      if class_idx is not None:
-          print ('warn: --class=lbl ignored when running on an unconditional network')
-  return label
+    label = torch.zeros([1, G.c_dim], device=device)
+    if G.c_dim != 0:
+        if class_idx is None:
+            ctx.fail('Must specify class label with --class when using a conditional network')
+        label[:, class_idx] = 1
+    else:
+        if class_idx is not None:
+            print('warn: --class=lbl ignored when running on an unconditional network')
+    return label
 
 def generate_image(device, G, z, truncation_psi=1.0, noise_mode='const', class_idx=None):
-  z = torch.from_numpy(z).to(device)
-  label = get_label(G, device, class_idx)
-  img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
-  img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-  #PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
-  return PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
+    z = torch.from_numpy(z).to(device)
+    label = get_label(G, device, class_idx)
+    img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+    img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+    return PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB')
 
 def expand_seed(seeds, vector_size):
   result = []
-
   for seed in seeds:
     rnd = np.random.RandomState(seed)
-    result.append( rnd.randn(1, vector_size) )
+    result.append(rnd.randn(1, vector_size))
   return result
 
 print("\nLATENT VECTOR WALK VIDEO GENERATOR made by JUSTIN GALLAGHER")
 print("DO NOT INCLUDE QUOTATIONS IN ANY FILE OR DIRECTORY PATH NAMES!\n")
-URL = input('What is the .pkl file path: ')
+pkl_file = input('What is the .pkl file path: ')
 
-print(f'Loading networks from "{URL}"...')
+print(f'Loading networks from "{pkl_file}"...')
 device = torch.device('cuda')
-with dnnlib.util.open_url(URL) as f:
-    G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+with dnnlib.util.open_url(pkl_file) as f:
+    G = legacy.load_network_pkl(f)['G_ema'].to(device)
 
 vector_size = G.z_dim
-# range(8192,8300)
 seeds = expand_seed( [8192+1,8192+9], vector_size)
-#generate_images(Gs, seeds,truncation_psi=0.5)
-print(seeds[0].shape)
 
-# Choose your seeds to morph through and the number of steps to take to get to each.
+seed_list = []
+seed_num = int(input("How many seeds would you like to input: "))
+print('Begin inputting seeds now...')
+for i in range(0, seed_num):
+    seed = int(input(f'Seed #{i+1}: '))
+    seed_list.append(seed)
 
-SEEDS = []
-SEEDS_NUM = int(input("How many seeds would you like to input: "))
-for i in range(0, SEEDS_NUM):
-    SEED = int(input(f'Seed #{i+1}: '))
-    SEEDS.append(SEED)  # adding the individual seeds to seed list
+steps = int(input("Number of steps between seeds: "))
+save_path = input('Save video to which directory: ')
 
-STEPS = int(input("Number of steps between seeds: "))
-save_path = input('Save to which directory: ')
-
+print('Creating temp directory for images...')
 temp = os.path.join(save_path, "temp-images")
 if os.path.isdir(temp):
     print('Temp directory already exists. Deleting images in:')
@@ -90,26 +81,29 @@ if os.path.isdir(temp):
 else:
     os.mkdir(temp)
 
-# Generate the images for the video.
+print('Begin image generation...')
 idx = 0
-for i in range(len(SEEDS) - 1):
-    v1 = seed2vec(G, SEEDS[i])
-    v2 = seed2vec(G, SEEDS[i + 1])
+for i in range(len(seed_list) - 1):
+    v1 = seed2vec(G, seed_list[i])
+    v2 = seed2vec(G, seed_list[i + 1])
 
     diff = v2 - v1
-    step = diff / STEPS
+    step = diff / steps
     current = v1.copy()
 
-    for j in range(STEPS):
+    for j in range(steps):
         current = current + step
         img = generate_image(device, G, current)
         img.save(os.path.join(temp, f'frame-{idx}.png'))
         idx += 1
 
-    print(f"Completed Seed {SEEDS[i]}")
+print('Image generation complete...')
+print('Begin video processing...')
 
-# Link the images into a video.
+ffmpeg_cmd = 'ffmpeg -r 30 -i '+os.path.join(temp,'frame-%d.png')+' -vcodec mpeg4 -y '+os.path.join(save_path, "movie.mp4")+''
+os.system(ffmpeg_cmd)
 
-cmd = 'ffmpeg -r 30 -i '+os.path.join(temp,'frame-%d.png')+' -vcodec mpeg4 -y '+os.path.join(save_path, "movie.mp4")+''
-os.system(cmd)
+print("Removing all temporary files...")
+del_temp_images = 'rmdir /Q /S '+temp
+os.system(del_temp_images)
 print('Done.')
